660f56503b8f33f7a79d09e2f7500796bd9a2eb1
JoHoop <jolienhoop@gmail.com>
2021-02-20 15:19:34 +0100

    Add custom meta schema validation

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import Form from '@rjsf/material-ui';
+import metaSchemaDraft04 from 'ajv/lib/refs/json-schema-draft-04.json';
 import { schema } from '../data/schema';
 import { jsonData } from '../data/jsonData';
+
+console.log(JSON.stringify(schema));
+
+const uiSchema = {
+  $schema: { 'ui:widget': 'hidden' },
+  meta: { 'ui:widget': 'hidden' },
+};
 
 export const Editor = () => {
   const [formData, setFormData] = useState(jsonData);
@@ -22,10 +30,13 @@ export const Editor = () => {
   return (
     <Form
       schema={schema}
+      uiSchema={uiSchema}
       formData={formData}
       onChange={onChange}
       onSubmit={onSubmit}
       onError={onError}
+      additionalMetaSchemas={[metaSchemaDraft04]}
+      liveValidate
     />
   );
 };
