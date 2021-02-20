2b18edb5cf5e630888ed5f223458b536664c4cc6
JoHoop <jolienhoop@gmail.com>
2021-02-20 14:41:40 +0100

    Add form data and event listeners

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import Form from '@rjsf/material-ui';
 import { schema } from '../data/schema';
+import { jsonData as formData } from '../data/jsonData';
 
 export const Editor = () => {
-  return <Form schema={schema} />;
+  const onChange = ({ formData }) => {
+    console.log('onChange: ', formData);
+  };
+
+  const onSubmit = (value) => {
+    console.log('onSubmit: %s', JSON.stringify(value));
+  };
+
+  const onError = () => {
+    console.log('errors');
+  };
+
+  return (
+    <Form
+      schema={schema}
+      formData={formData}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      onError={onError}
+    />
+  );
 };
