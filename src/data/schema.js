ab5719c988bea3b0db7a97f520737159fa0265e8
JoHoop <jolienhoop@gmail.com>
2021-02-20 14:24:59 +0100

    Add resume schema

---
+++ src/data/schema.js
@@ -0,0 +1,17 @@
+import resumeSchema from 'resume-schema';
+
+export const validate = () => {
+  resumeSchema.validate(
+    { name: 'Thomas' },
+    function (err, report) {
+      if (err) {
+        console.error('The resume was invalid:', err);
+        return;
+      }
+      console.log('Resume validated successfully:', report);
+    },
+    function (err) {
+      console.error('The resume was invalid:', err);
+    }
+  );
+};
