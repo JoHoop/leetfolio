bdafb90d712e370ba83745367b54897fb5da169d
JoHoop <jolienhoop@gmail.com>
2021-02-22 17:11:29 +0100

    Add Firebase sign in

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState, useCallback, useContext } from 'react';
 import { Redirect } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import { signIn } from '../services/Auth.js';
 import { UserContext } from '../services/UserProvider.js';
-import { isEmailValid, hasEmptyElement } from '../services/Validators';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import { UseForm } from '../components/UseForm';
+import { isEmailValid } from '../services/Validators';
+import { Loading } from '../components/Loading';
+import {
+  Avatar,
+  Snackbar,
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  makeStyles,
+} from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -31,15 +35,58 @@ const useStyles = makeStyles((theme) => ({
   },
   form: {
     width: '100%',
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
 }));
 
+const Alert = (props) => {
+  return <MuiAlert elevation={6} variant='filled' {...props} />;
+};
+
 export const SignIn = () => {
   const classes = useStyles();
+  const [values, handleChange] = UseForm({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = values;
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const showError = (error) => {
+    setErrorMessage(`${error.message} (Code: ${error.code})`);
+  };
+
+  const resetError = () => {
+    setErrorMessage('');
+  };
+
+  const handleSignIn = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      await signIn(email, password);
+      console.log('success');
+      return <Redirect to='/account' />;
+    } catch (error) {
+      console.log(error);
+      showError(error);
+    }
+    setIsLoading(false);
+  }, [email, password]);
+
+  const { currentUser } = useContext(UserContext);
+
+  if (currentUser) {
+    return <Redirect to='/account' />;
+  }
+
+  const emailInputValid = () => {
+    return email === '' || isEmailValid(email);
+  };
 
   return (
     <Container component='main' maxWidth='xs'>
@@ -52,40 +99,45 @@ export const SignIn = () => {
           Sign in
         </Typography>
         <form className={classes.form} noValidate>
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            id='email'
-            label='Email Address'
-            name='email'
-            autoComplete='email'
-            autoFocus
-          />
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            autoComplete='current-password'
-          />
-          <FormControlLabel
-            control={<Checkbox value='remember' color='primary' />}
-            label='Remember me'
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                id='email'
+                label='Email'
+                name='email'
+                autoComplete='email'
+                value={email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                autoComplete='current-password'
+                value={password}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
           <Button
             type='submit'
             fullWidth
             variant='contained'
             color='primary'
             className={classes.submit}
+            disabled={!emailInputValid}
+            onClick={() => handleSignIn()}
           >
-            Sign In
+            Sign in
           </Button>
           <Grid container>
             <Grid item xs>
@@ -101,6 +153,16 @@ export const SignIn = () => {
           </Grid>
         </form>
       </Box>
+      {isLoading && <Loading />}
+      <Snackbar
+        open={errorMessage !== ''}
+        autoHideDuration={6000}
+        onClose={resetError}
+      >
+        <Alert onClose={resetError} severity='success'>
+          This is a success message!
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
