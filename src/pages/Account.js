cdc212db254a8649e3c6219ee13c828e35574a3f
JoHoop <jolienhoop@gmail.com>
2021-02-22 17:55:49 +0100

    Implement Firebase sign out

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router';
+import Firebase from '../services/Firebase';
+import {
+  signOut,
+  changeUsername,
+  changeEmail,
+  changePassword,
+  changePhoto,
+  deleteUser,
+  resetPassword,
+  verifyEmail,
+} from '../services/Auth.js';
+
+import { UserContext } from '../services/UserProvider.js';
+import { Button, Typography } from '@material-ui/core';
 
 export const Account = () => {
+  const { currentUser } = useContext(UserContext);
+
+  const handleSignOut = async () => {
+    signOut();
+    return <Redirect to='/signin' />;
+  };
+
   return (
     <React.Fragment>
       <Typography component='h1' variant='h2' color='textPrimary' gutterBottom>
         Account
       </Typography>
       <Typography variant='h5' color='textSecondary' paragraph>
-        Account
+        Hey, {currentUser.email}
       </Typography>
+      <Button color='default' onClick={handleSignOut}>
+        Sign out
+      </Button>
     </React.Fragment>
   );
 };
