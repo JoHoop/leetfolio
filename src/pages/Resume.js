b07467a4319535b0444937b0bda4d8425ae30343
JoHoop <jolienhoop@gmail.com>
2021-02-24 22:29:08 +0100

    Draft fullscren preview for resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,6 +1,18 @@
 import React, { useContext } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
 import { ResumeContext } from '../components/Resume/ResumeProvider';
 import { Page } from '../components/Resume/Page';
 import { Card } from '../components/Resume/Card';
@@ -117,6 +129,46 @@ export const Resume = () => {
           </p>
         </div>
       </Page>
+    </React.Fragment>
+  );
+};
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />;
+});
+
+export const Fullscreen = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant='outlined' color='primary' onClick={handleClickOpen}>
+        Open full-screen dialog
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <IconButton
+          edge='start'
+          color='inherit'
+          onClick={handleClose}
+          aria-label='close'
+        >
+          <CloseIcon />
+        </IconButton>
+        <Resume />
+      </Dialog>
     </React.Fragment>
   );
 };
