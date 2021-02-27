14041de5e72d83daaece584b5786b5ff34b9ebd1
JoHoop <jolienhoop@gmail.com>
2021-02-27 10:44:23 +0100

    Add landing page illustration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, makeStyles } from '@material-ui/core';
+import OnlineResumeIllustration from '../data/onlineResume.svg';
+
+const useStyles = makeStyles((theme) => ({
+  illustration: {
+    width: '100%',
+  },
+}));
 
 export const Home = () => {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <Typography component='h1' variant='h2' color='textPrimary' gutterBottom>
-        Home
+        LeetFolio
       </Typography>
       <Typography variant='h5' color='textSecondary' paragraph>
-        Home
+        LeetFolio
       </Typography>
+      <img
+        src={OnlineResumeIllustration}
+        className={classes.illustration}
+        alt='LeetFolio Logo'
+      />
     </React.Fragment>
   );
 };
